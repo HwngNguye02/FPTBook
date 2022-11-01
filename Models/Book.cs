d066f6266905442fbafe1f91ab121e2aae7d048e
HwngNguye02 <105761595+HwngNguye02@users.noreply.github.com>
2022-11-01 16:53:36 +0700

    Add files via upload

---
+++ Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASM1670.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        [MinLength(5, ErrorMessage = "Name length must be at least 5 characters")]
+        public string Name { get; set; }
+        [Required]
+        [Range(10000, 1000000, ErrorMessage = "Price must be from 10000 to 1.000.000")]
+        public double Price { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        [Required]
+        public string Image { get; set; }        
+        public string Language { get; set; }
+        [Required]
+        [Range(1, 100, ErrorMessage = "Quanity must be from 1 to 100")]
+        public int Quantity { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace ASM1670.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string UserEmail { get; set; }
+        [Required]
+        public int OrderQuantity { get; set; }
+        public double OrderPrice { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime OrderDate { get; set; }
+        public int BookId { get; set; }   //frkey
+        public Book Book { get; set; }
+    }
+}
